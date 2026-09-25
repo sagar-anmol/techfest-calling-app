3adbf4bce4298518a54ca3496eb07f4a31427fa3
sagar-anmol <sagarkumarweebly@gmail.com>
2026-09-26 03:15:47 +0530

    feat: Add PWA installability and 1-click WhatsApp instant login

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'tf26-calling-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((k) => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
